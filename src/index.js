8c42635f6a5e06ed0b519fa8d552e2f12b4dfd9c
Max Daunarovich <max.daunarovich@gmail.com>
2020-10-01 19:52:48 +0300

    Setup basic export file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { get, set, getConfigValue } from "./utils/config";
+export { getTemplate } from "./utils/file";
+export { sendTransaction, executeScript } from "./utils/interaction";
+export { mintFlow, getFlowBalance } from "./utils/flow-token";
+export { deployContract } from "./utils/deploy-code";
+export { createAccount } from "./utils/create-account";
