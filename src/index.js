239026a174476683e7acafa14b70d50da3a512eb
Max Daunarovich <max.daunarovich@gmail.com>
2020-10-06 19:56:19 +0300

    Update exports

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export { getTemplate } from "./utils/file";
 export { sendTransaction, executeScript } from "./utils/interaction";
 export { mintFlow, getFlowBalance } from "./utils/flow-token";
 export { deployContract } from "./utils/deploy-code";
-export { createAccount } from "./utils/create-account";
+export { getAccount } from "./utils/create-account";
+export { init } from "./utils/init";
