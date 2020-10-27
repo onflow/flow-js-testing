c6a850a2a3f1597048fe8b189be43f9396b2c633
Bastian Müller <bastian@axiomzen.co>
2020-10-27 15:07:53 -0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
