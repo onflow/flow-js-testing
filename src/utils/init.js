9ffc920260da531721e5c5e12762526e621c452e
Max Daunarovich <max.daunarovich@gmail.com>
2021-04-16 16:44:18 +0300

    Use updated path for service account private key

--- src/utils/init.js
+++ src/utils/init.js
@@ -19,11 +19,11 @@
 import { set } from "./config";
 
 export const init = async (basePath) => {
-  set("PRIVATE_KEY", process.env.PK, "accounts/service/privateKey");
+  set("PRIVATE_KEY", process.env.PK, "accounts/emulator-account/keys");
   set(
     "SERVICE_ADDRESS",
     process.env.SERVICE_ADDRESS,
-    "accounts/service/address",
+    "accounts/emulator-account/address",
     "f8d6e0586b0a20c7"
   );
   set(
