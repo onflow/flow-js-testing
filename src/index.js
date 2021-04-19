859497356f89f65fec8cf77b81c4c9b5ac01b321
Max Daunarovich <max.daunarovich@gmail.com>
2021-04-19 19:00:04 +0300

    Adjust exports

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@
  */
 
 export { init } from "./utils/init";
-export { get, set, getConfigValue } from "./utils/config";
+export { set, getConfigValue } from "./utils/config";
 export { getTemplate, getScriptCode, getContractCode, getTransactionCode } from "./utils/file";
 export { sendTransaction, executeScript } from "./utils/interaction";
-export { mintFlow, getFlowBalance } from "./utils/flow-token";
+export { getFlowBalance, mintFlow } from "./utils/flow-token";
 export { deployContract, deployContractByName } from "./utils/deploy-code";
 export { getAccountAddress } from "./utils/create-account";
 export { getContractAddress } from "./utils/contract";
