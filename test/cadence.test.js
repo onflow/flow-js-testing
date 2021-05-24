ad5191c3235ef899b689b828e19ffc12996d406a
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-24 09:26:04 +0300

    Scaffold test for loading service templates

---
+++ test/cadence.test.js
@@ -0,0 +1,8 @@
+import { getScriptCode } from "../src";
+
+describe("manager code", () => {
+  test("FlowManager contract", async () => {
+    const code = "implement-geneartor";
+    expect(code).toBe("");
+  });
+});
