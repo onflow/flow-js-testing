4d092481f8d213b9c369eacee1bee5a032c90ba8
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-01 18:30:46 +0300

    Add test to ensure templates are working

--- test/cadence.test.js
+++ test/cadence.test.js
@@ -1,8 +1,11 @@
-import { getScriptCode } from "../src";
+import registry from "../src/generated";
+const { FlowManagerTemplate } = registry.contracts;
 
 describe("manager code", () => {
   test("FlowManager contract", async () => {
-    const code = "implement-geneartor";
-    expect(code).toBe("");
+
+    const code = await FlowManagerTemplate()
+
+    expect(code.includes("pub contract FlowManager")).toBe(true);
   });
 });
