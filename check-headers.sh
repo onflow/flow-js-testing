31c548e6b016ac06db43e081bae8f8e126e6399c
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-03 03:04:14 +0300

    Update script to exclude generated folder

--- check-headers.sh
+++ check-headers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=$(find src -name \*.js -type f -print0 | xargs -0 egrep -L '(Licensed under the Apache License)|(Code generated from|by)')
+files=$(find ./src -path ./src/generated -prune -false -o -name \*.js -type f -print0 | xargs -0 egrep -L '(Licensed under the Apache License)|(Code generated from|by)')
 if [ -n "$files" ]; then
   echo "Missing license header in:"
   echo "$files"
