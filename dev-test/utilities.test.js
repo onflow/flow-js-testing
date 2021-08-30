e9316b981cd2f2e6a5889cda37717a542b69b8c3
Max Daunarovich <max.daunarovich@gmail.com>
2021-08-30 20:18:29 +0300

    Update utilities tests

--- dev-test/utilities.test.js
+++ dev-test/utilities.test.js
@@ -2,7 +2,6 @@ import path from "path";
 import {
   emulator,
   init,
-  deployContractByName,
   getServiceAddress,
   shallPass,
   executeScript,
@@ -17,10 +16,9 @@ jest.setTimeout(10000);
 describe("interactions - sendTransaction", () => {
   // Instantiate emulator and path to Cadence files
   beforeEach(async () => {
-    const contracts = path.resolve(__dirname, "../cadence/contracts");
-    const base = path.resolve(__dirname, "./cadence");
+    const base = path.resolve(__dirname, "../cadence");
     const port = 8080;
-    await init({ base, contracts }, { port, loadManager: false });
+    await init({ base }, { port, loadManager: false });
     return emulator.start(port);
   });
 
@@ -29,29 +27,19 @@ describe("interactions - sendTransaction", () => {
     return emulator.stop();
   });
 
-  it("should deploy TestUtilities contract", async () => {
-    const to = await getServiceAddress();
-    await shallPass(deployContractByName({ name: "FlowManager", to }));
-  });
-
   it("should return zero offset", async () => {
-    const to = await getServiceAddress();
-    await shallPass(deployContractByName({ name: "FlowManager", to }));
-
     const result = await executeScript("get-block-offset");
     expect(result).toBe(0);
   });
 
   it("should update offset", async () => {
-    const to = await getServiceAddress();
-    await shallPass(deployContractByName({ name: "FlowManager", to }));
-
+    const manager = await getServiceAddress();
     const result = await executeScript("get-block-offset");
     expect(result).toBe(0);
 
     const offset = 42;
-    await shallPass(sendTransaction("set-offset", [to], [offset]));
-    const newOffset = await executeScript("read-offset");
+    await shallPass(sendTransaction("set-block-offset", [manager], [offset]));
+    const newOffset = await executeScript("get-block-offset");
     expect(newOffset).toBe(offset);
   });
 });
