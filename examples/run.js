c95228a593478dfa7c3349ae87bac4eb62a4db41
Max Daunarovich <max.daunarovich@gmail.com>
2021-09-23 02:31:21 +0300

    Update runner to include list of examples and allow to run by number

--- examples/run.js
+++ examples/run.js
@@ -1,3 +1,49 @@
-const [example] = process.argv.slice(2);
-// TODO: add check that file exists
-require("esm")(module /*, options*/)(example);
+const path = require("path");
+const fs = require("fs");
+
+// Regexp to match files in folder
+const exampleMatcher = /(\.\/)?\w{2}-.*\.js/g;
+
+const printTitle = (exampleName, padSymbol) => {
+  if (exampleName.endsWith(".js")) {
+    exampleName = exampleName.slice(0, -3);
+  }
+  const fixedName = exampleName
+    .split("-")
+    .map((item, i) => {
+      return i === 0 ? `#${item} -` : item[0].toUpperCase() + item.slice(1);
+    })
+    .join(" ");
+  const title = `Launching Example ${fixedName}`;
+  const divider = "".padEnd(title.length + 4, padSymbol);
+
+  console.log(divider);
+  console.log(title);
+  console.log(divider);
+};
+
+fs.readdir(__dirname, (err, files) => {
+  const examples = files.filter((file) => file.match(exampleMatcher));
+  let [exampleName] = process.argv.slice(2);
+
+  let filepath;
+  if (exampleName.match(exampleMatcher)) {
+    filepath = path.resolve(__dirname, exampleName);
+  } else {
+    const name = examples.find((item) => item.includes(exampleName));
+    if (!name) {
+      filepath = null;
+    } else {
+      filepath = path.resolve(__dirname, name);
+    }
+  }
+
+  if (filepath) {
+    printTitle(exampleName, "=");
+    // require file and execute it
+    require("esm")(module /*, options*/)(filepath);
+  } else {
+    console.log(`Example "${exampleName}" not found!\n`);
+    console.log("Try one of available examples:", examples);
+  }
+});
