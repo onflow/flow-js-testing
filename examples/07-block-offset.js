bc60c38e6fad2360a426a78c77805d65820c2488
Max Daunarovich <max.daunarovich@gmail.com>
2021-09-27 19:40:26 +0300

    Refactor block offset example

--- examples/07-block-offset.js
+++ examples/07-block-offset.js
@@ -6,25 +6,17 @@ import {
   setBlockOffset,
   builtInMethods,
   executeScript,
-  getAccountAddress,
 } from "../src";
 
 (async () => {
-  console.log("/home/max")
   const basePath = path.resolve(__dirname, "./cadence");
   const port = 8080;
 
   await init(basePath, { port });
   await emulator.start(port);
 
-  const Alice = await getAccountAddress("Alice");
-  console.log({ Alice });
-
-  // Offset current block height by 42
-  await setBlockOffset(42);
-
-  const blockOffset = await getBlockOffset();
-  console.log({ blockOffset });
+  const initialBlockOffset = await getBlockOffset();
+  console.log({ initialBlockOffset });
 
   // "getCurrentBlock().height" in your Cadence code will be replaced by Manager to a mocked value
   const code = `
@@ -33,12 +25,22 @@ import {
     }
   `;
 
+  // We can check that non-transformed code still works just fine
+  const normalResult = await executeScript({ code });
+  console.log({ normalResult });
+
+  // Offset current block height by 42
+  await setBlockOffset(42);
+  // Let's check that offset value on Manager is actually changed to 42
+  const blockOffset = await getBlockOffset();
+  console.log({ blockOffset });
+
   // "transformers" field expects array of functions to operate update the code.
-  // We will pass single operator "builtInMethods" provided by the framework
+  // We will pass single operator "builtInMethods" provided by the framework to alter how getCurrentBlock().height is calculated
   const transformers = [builtInMethods];
-  const result = await executeScript({ code, transformers });
-  console.log({ result });
+  const transformedResult = await executeScript({ code, transformers });
+  console.log({ transformedResult });
 
+  // Stop the emulator
   await emulator.stop();
-
 })();
