4155a6c118606d26801ea753e6b2f1c2e097aa49
Max Daunarovich <max.daunarovich@gmail.com>
2021-11-19 10:34:42 +0300

    Clear second transaction

--- src/generated/transactions/deployContract.js
+++ src/generated/transactions/deployContract.js
@@ -9,26 +9,7 @@ import {
 } from 'flow-cadut'
 
 export const CODE = `
-  import FlowManager from 0x01
-
-transaction(name:String, code: String, manager: Address ##ARGS-WITH-TYPES##) {
-    prepare(acct: AuthAccount){
-        let decoded = code.decodeHex()
-        acct.contracts.add(
-           name: name,
-           code: decoded,
-           ##ARGS-LIST##
-        )
-
-        let linkPath = FlowManager.contractManagerPath
-        let contractManager = getAccount(manager)
-                    .getCapability(linkPath)!
-                    .borrow<&FlowManager.Mapper>()!
-
-        let address = acct.address
-        contractManager.setAddress(name, address: address)
-    }
-}
+import FlowManager from 0x01
 
 transaction(name:String, code: String, manager: Address ##ARGS-WITH-TYPES##) {
     prepare(acct: AuthAccount){
