8a5ce8eb0a6f69e5abd94cb2e50ef8cd2721795c
Anil Dhurjaty <adhurjaty@gmail.com>
2022-01-10 18:11:35 -0800

    trying to add contracts to service account

--- src/emulator.js
+++ src/emulator.js
@@ -83,7 +83,8 @@ export class Emulator {
 
     this.logging = logging;
     this.filters = [];
-    this.process = spawn("flow", ["emulator", "-v", "--http-port", port, "--port", grpc]);
+    console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&7")
+    this.process = spawn("flow", ["emulator", "-v", "--http-port", port, "--port", grpc, "--contracts"]);
     this.logProcessor = (item) => item;
 
     return new Promise((resolve, reject) => {
