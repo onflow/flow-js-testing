88bbe8d15d7005c953408a3f23d884f32dc2b2c7
Anil Dhurjaty <adhurjaty@gmail.com>
2022-01-13 19:39:12 -0800

    fix incorrect naming error

--- src/fusd-token.js
+++ src/fusd-token.js
@@ -24,7 +24,7 @@ import { getTokenBalance, mintToken, setupVault } from "./token";
  * @returns {Promise<*>}
  */
 export const getFUSDBalance = async (address) => {
-  await setupVault("FUSD", recipient);
+  await setupVault("FUSD", address);
   return await getTokenBalance("FUSD", address);
 };
 
