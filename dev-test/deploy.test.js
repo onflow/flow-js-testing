b195d442266b846891e4d7a32b40cb4b6724786f
Max Daunarovich <max.daunarovich@gmail.com>
2022-02-24 17:45:57 +0300

    Fix deployment tests

--- dev-test/deploy.test.js
+++ dev-test/deploy.test.js
@@ -3,10 +3,13 @@ import {
   emulator,
   init,
   executeScript,
+  deployContract,
   deployContractByName,
   getContractAddress,
   getAccountAddress,
-  getServiceAddress
+  getServiceAddress,
+  shallPass,
+  shallResolve,
 } from "../src";
 
 // We need to set timeout for a higher number, cause some transactions might take up some time
@@ -29,7 +32,7 @@ describe("interactions - sendTransaction", () => {
   test("deploy basic contract - to service account", async () => {
     const name = "HelloWorld";
     await deployContractByName({ name });
-    const [ address ] = await getContractAddress(name);
+    const address = await getContractAddress(name);
     const serviceAccount = await getServiceAddress();
     expect(address).toBe(serviceAccount);
   });
@@ -37,7 +40,7 @@ describe("interactions - sendTransaction", () => {
   test("deploy basic contract - to service account, short notation", async () => {
     const name = "HelloWorld";
     await deployContractByName(name);
-    const [ address ] = await getContractAddress(name);
+    const address = await getContractAddress(name);
     const serviceAccount = await getServiceAddress();
     expect(address).toBe(serviceAccount);
   });
@@ -46,7 +49,7 @@ describe("interactions - sendTransaction", () => {
     const Alice = await getAccountAddress("Alice");
     const name = "HelloWorld";
     await deployContractByName({ name, to: Alice });
-    const [ address ] = await getContractAddress(name);
+    const address = await getContractAddress(name);
     expect(address).toBe(Alice);
   });
 
@@ -54,7 +57,7 @@ describe("interactions - sendTransaction", () => {
     const name = "HelloWorld";
     const Alice = await getAccountAddress("Alice");
     await deployContractByName(name, Alice);
-    const [ address ] = await getContractAddress(name);
+    const address = await getContractAddress(name);
     expect(address).toBe(Alice);
   });
 
@@ -68,9 +71,59 @@ describe("interactions - sendTransaction", () => {
         pub fun main():String{
           return HelloWorld.message
         }
-      `
-    })
-    expect(err).toBe(null)
-    expect(result).toBe("Hello, from Cadence")
+      `,
+    });
+    expect(result).toBe("Hello, from Cadence");
+    expect(err).toBe(null);
+  });
+
+  test("deploy custom contract with arguments", async () => {
+    const message = "Hello, Cadence";
+    const number = 42;
+    await shallPass(
+      deployContract({
+        code: `
+        pub contract Basic{
+          pub let message: String
+          pub let number: Int
+          init(message: String, number: Int){
+            self.message = message
+            self.number = number
+          }
+        }
+      `,
+        args: [message, number],
+      }),
+    );
+
+    // Read message
+    const [messageResult, messageErr] = await shallResolve(
+      executeScript({
+        code: `
+        import Basic from 0x1
+        
+        pub fun main():String{
+          return Basic.message
+        }
+      `,
+      }),
+    );
+    expect(messageResult).toBe(message);
+    expect(messageErr).toBe(null);
+
+    // Read number
+    const [numberResult, numberErr] = await shallResolve(
+      executeScript({
+        code: `
+        import Basic from 0x1
+        
+        pub fun main():Int{
+          return Basic.number
+        }
+      `,
+      }),
+    );
+    expect(numberResult).toBe(number);
+    expect(numberErr).toBe(null);
   });
 });
