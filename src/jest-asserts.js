da64ddf0682bd4ff46e833758d0615446aa1b1d0
Max Daunarovich <max.daunarovich@gmail.com>
2022-02-24 16:57:37 +0300

    refactor jest asserts

--- src/jest-asserts.js
+++ src/jest-asserts.js
@@ -38,12 +38,13 @@ export const promise = async (ix) => {
 export const shallPass = async (ix) => {
   const wrappedInteraction = promise(ix);
 
-  let result = await wrappedInteraction;
+  const response = await wrappedInteraction;
+  const [result] = response
 
   let resolvedStatus;
   let resolvedErrorMessage;
   if (Array.isArray(result)) {
-    const { status, errorMessage } = result[0];
+    const { status, errorMessage } = result;
     resolvedStatus = status;
     resolvedErrorMessage = errorMessage;
   } else {
@@ -55,7 +56,7 @@ export const shallPass = async (ix) => {
   await expect(resolvedStatus).toBe(4);
   await expect(resolvedErrorMessage).toBe("");
 
-  return promise(ix);
+  return response;
 };
 
 /**
@@ -80,9 +81,12 @@ export const shallResolve = async (ix) => {
  * */
 export const shallRevert = async (ix) => {
   const wrappedInteraction = promise(ix);
-  const [result, error] = await wrappedInteraction;
+  const response = await wrappedInteraction;
+  const [result, error] = response
   await expect(result).toBe(null);
   await expect(error).not.toBe(null);
+
+  return response
 };
 
 /**
@@ -92,7 +96,11 @@ export const shallRevert = async (ix) => {
  * */
 export const shallThrow = async (ix) => {
   const wrappedInteraction = promise(ix);
-  const [result, error] = await wrappedInteraction;
+  const response = await wrappedInteraction;
+
+  const [result, error] = response
   await expect(result).toBe(null);
   await expect(error).not.toBe(null);
+
+  return response;
 };
