2082243fb74fdf69b98a79ca309f002acf565268
Jordan Ribbink <jribbink@telus.net>
2022-07-29 08:44:45 -0700

    Add non nil assert

--- cadence/transactions/create-account.cdc
+++ cadence/transactions/create-account.cdc
@@ -15,7 +15,7 @@ transaction (_ name: String?, pubKey: [String], manager: Address) {
             
             // Create a record in account database
             let address = newAccount.address
-            accountManager.setAddress(name, address: address)
+            accountManager.setAddress(name!, address: address)
         }
     }
 }
--- src/generated/transactions/createAccount.js
+++ src/generated/transactions/createAccount.js
@@ -12,23 +12,23 @@ export const CODE = `
 import FlowManager from 0x01
 
 transaction (_ name: String?, pubKey: [String], manager: Address) {
-  prepare( admin: AuthAccount) {
-      let newAccount = AuthAccount(payer:admin)
-      for key in pubKey {
-          newAccount.addPublicKey(key.decodeHex())
-      }
+    prepare( admin: AuthAccount) {
+        let newAccount = AuthAccount(payer:admin)
+        for key in pubKey {
+            newAccount.addPublicKey(key.decodeHex())
+        }
 
-      if name != nil {
-          let linkPath = FlowManager.accountManagerPath
-          let accountManager = getAccount(manager)
-                              .getCapability(linkPath)!
-                              .borrow<&FlowManager.Mapper>()!
-          
-          // Create a record in account database
-          let address = newAccount.address
-          accountManager.setAddress(name, address: address)
-      }
-  }
+        if name != nil {
+            let linkPath = FlowManager.accountManagerPath
+            let accountManager = getAccount(manager)
+                                .getCapability(linkPath)!
+                                .borrow<&FlowManager.Mapper>()!
+            
+            // Create a record in account database
+            let address = newAccount.address
+            accountManager.setAddress(name!, address: address)
+        }
+    }
 }
 
 `;
