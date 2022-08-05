962b53572848ba17f7b472e07171f0e775448406
Jordan Ribbink <jribbink@users.noreply.github.com>
2022-08-05 14:52:23 -0700

    Bump @onflow/flow-cadut to 0.2.0-alpha.7 (#164)

---
+++ .changeset/friendly-snails-push.md
@@ -0,0 +1,5 @@
+---
+"@onflow/flow-js-testing": patch
+---
+
+Bump @onflow/flow-cadut to 0.2.0-alpha.7 (fixes bug where optional array, dictionary, path arguments did not work)
--- src/generated/scripts/index.js
+++ src/generated/scripts/index.js
@@ -1,24 +1,40 @@
 import { checkManagerTemplate, checkManager } from "./checkManager";
-import { getAccountAddressTemplate, getAccountAddress } from "./getAccountAddress";
-import { getBalanceTemplate, getBalance } from "./getBalance";
-import { getBlockOffsetTemplate, getBlockOffset } from "./getBlockOffset";
-import { getContractAddressTemplate, getContractAddress } from "./getContractAddress";
-import { getManagerAddressTemplate, getManagerAddress } from "./getManagerAddress";
-import { getTimestampOffsetTemplate, getTimestampOffset } from "./getTimestampOffset";
-
-export default {
-  checkManagerTemplate,
-  checkManager,
-  getAccountAddressTemplate,
-  getAccountAddress,
-  getBalanceTemplate,
-  getBalance,
-  getBlockOffsetTemplate,
-  getBlockOffset,
-  getContractAddressTemplate,
-  getContractAddress,
-  getManagerAddressTemplate,
-  getManagerAddress,
-  getTimestampOffsetTemplate,
-  getTimestampOffset,
-};
+	import { getAccountAddressTemplate, getAccountAddress } from "./getAccountAddress";
+	import { getBalanceTemplate, getBalance } from "./getBalance";
+	import { getBlockOffsetTemplate, getBlockOffset } from "./getBlockOffset";
+	import { getContractAddressTemplate, getContractAddress } from "./getContractAddress";
+	import { getManagerAddressTemplate, getManagerAddress } from "./getManagerAddress";
+	import { getTimestampOffsetTemplate, getTimestampOffset } from "./getTimestampOffset";
+	export {
+	  checkManagerTemplate,
+	  checkManager,
+	  getAccountAddressTemplate,
+	  getAccountAddress,
+	  getBalanceTemplate,
+	  getBalance,
+	  getBlockOffsetTemplate,
+	  getBlockOffset,
+	  getContractAddressTemplate,
+	  getContractAddress,
+	  getManagerAddressTemplate,
+	  getManagerAddress,
+	  getTimestampOffsetTemplate,
+	  getTimestampOffset,
+	};
+	export default {
+	  checkManagerTemplate,
+	  checkManager,
+	  getAccountAddressTemplate,
+	  getAccountAddress,
+	  getBalanceTemplate,
+	  getBalance,
+	  getBlockOffsetTemplate,
+	  getBlockOffset,
+	  getContractAddressTemplate,
+	  getContractAddress,
+	  getManagerAddressTemplate,
+	  getManagerAddress,
+	  getTimestampOffsetTemplate,
+	  getTimestampOffset,
+	};
+	
