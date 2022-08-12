3f616a78aa175d6a863b9db429d2f9527387f073
Jordan Ribbink <jribbink@users.noreply.github.com>
2022-08-12 09:32:53 -0700

    Remove redundant flow-cadut functions and export from there instead (#167)

--- src/imports.js
+++ src/imports.js
@@ -18,64 +18,7 @@
 
 import {getContractAddress} from "./contract"
 import {defaultsByName} from "./file"
-
-const REGEXP_IMPORT = /(\s*import\s*)([\w\d]+)(\s+from\s*)([\w\d".\\/]+)/g
-
-const getPairs = line => {
-  return line
-    .split(/\s/)
-    .map(item => item.replace(/\s/g, ""))
-    .filter(item => item.length > 0 && item !== "import" && item !== "from")
-}
-
-const collect = (acc, item) => {
-  const [contract, address] = item
-  acc[contract] = address
-  return acc
-}
-
-/**
- * Returns address map for contracts defined in template code.
- * @param {string} code - Cadence code to parse.
- * @returns {*}
- */
-export const extractImports = code => {
-  if (!code || code.length === 0) {
-    return {}
-  }
-  return code
-    .split("\n")
-    .filter(line => line.includes("import"))
-    .map(getPairs)
-    .reduce(collect, {})
-}
-
-export const replaceImports = (code, addressMap) => {
-  return code.replace(REGEXP_IMPORT, (match, imp, contract) => {
-    const newAddress =
-      addressMap instanceof Function
-        ? addressMap(contract)
-        : addressMap[contract]
-    return `${imp}${contract} from ${newAddress}`
-  })
-}
-
-/**
- * Returns Cadence template code with replaced import addresses
- * @param {string} code - Cadence template code.
- * @param {{string:string}} [addressMap={}] - name/address map or function to use as lookup table
- * for addresses in import statements.
- * @param byName - lag to indicate whether we shall use names of the contracts.
- * @returns {*}
- */
-export const replaceImportAddresses = (code, addressMap, byName = true) => {
-  return code.replace(REGEXP_IMPORT, (match, imp, contract, _, address) => {
-    const key = byName ? contract : address
-    const newAddress =
-      addressMap instanceof Function ? addressMap(key) : addressMap[key]
-    return `${imp}${contract} from ${newAddress}`
-  })
-}
+import {replaceImportAddresses, extractImports} from "@onflow/flow-cadut"
 
 /**
  * Resolves import addresses defined in code template
@@ -95,3 +38,5 @@ export const resolveImports = async code => {
   }
   return addressMap
 }
+
+export {replaceImportAddresses, extractImports}
